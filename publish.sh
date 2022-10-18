9caa0539cbe75ac566f1ecc634dde92fbfde0e52
guanhaijie <>
2022-10-18 17:41:38 +0800

    ceshi

--- publish.sh
+++ publish.sh
@@ -3,7 +3,15 @@ read -p "输入更改理由：" str
 git add .
 git commit -m "$str"
 git push origin master
-
+ssh -p9022 9zcloud@192.168.2.50
+9zcloud
+git pull origin master
+cd /home/9zcloud/website
+npm run build
+var=$(cd "$(dirname "$0")";pwd)
+cd $var/build
+ps ax|grep "python3 -m https.server"|grep -v grep|awk '{print $1}'|xargs kill
+nohup python3 -m http.server 9988 > /tmp/doc.log 2>&1 &
 #获取路径
 #var=$(cd "$(dirname "$0")";pwd)
 #cd $var
