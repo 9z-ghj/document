153bef6219e79506abedc6364a71e8c6f5704e3b
guanhaijie <ghj@9z.cloud>
2022-10-18 03:33:03 -0700

    update .gitignore

--- publish.sh
+++ publish.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-git pull origin master
-
-#var=$(cd "$(dirname "$0")";pwd)
 while true
 do
+git pull origin master
+
+var=$(cd "$(dirname "$0")";pwd)
 npm run build
-echo -n "Still running at " data sleep 1800 done
-#cd $var/build
-#ps ax|grep "python3 -m https.server"|grep -v grep|awk '{print $1}'|xargs kill
-#nohup python3 -m http.server 9988 > /tmp/doc.log 2>&1 &
+cd $var/build
+ps ax|grep "python3 -m https.server"|grep -v grep|awk '{print $1}'|xargs kill
+nohup python3 -m http.server 9988 > /tmp/doc.log 2>&1 &
+echo -n "30分钟/次同步并发布中..." 
+sleep 1800
+done 
